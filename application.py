bb93716dd0874329d218f112aafb8eed8f8df04b
Edward Kang <edkang59@gmail.com>
2018-07-17 17:43:44 +0000

    message begin

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 import os
+import requests
 
-from flask import Flask
+from flask import Flask, jsonify, render_template, request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
@@ -10,6 +11,17 @@ socketio = SocketIO(app)
 # list of all channels
 channel_list = ['general']
 
+votes = []
+
 @app.route("/")
 def index():
-    return "Project 2: TODO"
+    return render_template("index.html", votes=votes)
+
+
+@socketio.on("submit vote")
+def vote(data):
+    selection = data["selection"]
+    print(selection)
+    votes.append(selection + '1234')
+    print(votes)
+    emit("vote totals", votes, broadcast=True)
