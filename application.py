497cc72019c9e897e739a1cd1235df5951df704f
Edward Kang <edkang59@gmail.com>
2018-07-18 16:53:35 +0000

    username duplicate checker, not working

--- application.py
+++ application.py
@@ -25,31 +25,46 @@ votes = ["what"]
 @app.route("/")
 def index():
     username = session.get('user')
-    print(users)
+    print("list of users (1): " + str(users))
     if username:
         login_status = "Yes"
-        print(login_status)
         return render_template("index.html", username=username, login_status=login_status, votes=votes)
     else:
         username = "blank"
         login_status = "No"
-        print(login_status)
-        return render_template("index.html", username=username, login_status=login_status, votes=votes)
+        login_error = "No"
+        return render_template("index.html", username=username, login_status=login_status, login_error=login_error, votes=votes)
+
 
 #Get input value from login.html and check if given username/password exist in db.
 @app.route("/loginPost", methods=['POST'])
 def loginPost():
     username = request.form.get("username")
-    users.append(username)
-    print(users)
-    session['user'] = username
-    return redirect(url_for('index', username=username))
+    if username in users:
+        username = "blank"
+        login_status = "No"
+        login_error = "Yes"
+        print("list of users (2): " + str(users))
+        print("login status: " + login_status)
+        print("login error: " + login_error)
+        return redirect(url_for('index', username=username, login_status=login_status, login_error=login_error, votes=votes))
+    else:
+        users.append(username)
+        session['user'] = username
+        login_status = "Yes"
+        login_error = "No"
+        print("list of users (2): " + str(users))
+        print("login status: " + login_status)
+        print("login error: " + login_error)
+        return redirect(url_for('index', username=username, login_status=login_status, login_error=login_error, votes=votes))
+
 
 #Logout user and redirect to '/' which in this case, login page.
 @app.route("/logout")
 def logout():
     session.pop('user', None)
     return redirect('/')
+
 
 @socketio.on("submit vote")
 def vote(data):
